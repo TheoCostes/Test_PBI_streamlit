e90fd6855fd2ab8df2e9f45c177f6bae7d5e9ae9
TheoCostes <78727529+TheoCostes@users.noreply.github.com>
2024-09-13 10:25:26 +0200

    Update PBI_in_streamlit.py

--- PBI_in_streamlit.py
+++ PBI_in_streamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title('Intégration de Power BI dans Streamlit')
 st.write('Voici un rapport Power BI intégré dans Streamlit')
 
-powerbi_url = 'https://app.powerbi.com/reportEmbed?reportId=ad9e2c1f-6437-43af-bcb5-9135a48d62ae&autoAuth=false&ctid=f85b23bd-1472-4658-b2cd-bff744680942'
+powerbi_url = 'https://app.powerbi.com/reportEmbed?reportId=ad9e2c1f-6437-43af-bcb5-9135a48d62ae&autoAuth=true&ctid=f85b23bd-1472-4658-b2cd-bff744680942'
 
 # Utilisation d'un iframe pour afficher le rapport Power BI
 st.components.v1.iframe(src=powerbi_url, width=1200, height=600)
